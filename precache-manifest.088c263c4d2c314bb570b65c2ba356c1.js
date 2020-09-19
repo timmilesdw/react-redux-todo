d4eb2ac92671241539dc71514d27d7ca30c46d4d
Tim <jsfire@google.com>
2020-09-20 01:30:37 +0500

    Updates

---
+++ precache-manifest.088c263c4d2c314bb570b65c2ba356c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41bc17ceac05e92f2003284bb9f1adc",
+    "url": "/react-redux-todo/index.html"
+  },
+  {
+    "revision": "227d1c5401929307dc1a",
+    "url": "/react-redux-todo/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "5586dfe4f75565de67d8",
+    "url": "/react-redux-todo/static/css/main.c627281a.chunk.css"
+  },
+  {
+    "revision": "227d1c5401929307dc1a",
+    "url": "/react-redux-todo/static/js/2.e470b724.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-redux-todo/static/js/2.e470b724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5586dfe4f75565de67d8",
+    "url": "/react-redux-todo/static/js/main.e3f696ac.chunk.js"
+  },
+  {
+    "revision": "1539bfa9ef8f23f0e793",
+    "url": "/react-redux-todo/static/js/runtime-main.ebec8b13.js"
+  },
+  {
+    "revision": "acec03b280d0fcd94e9a72d69619c9e9",
+    "url": "/react-redux-todo/static/media/logo192.acec03b2.png"
+  }
+]);
